d2afdc694aadb74e54b692adbffdcfa2a482b2c7
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2024-02-22 12:07:04 +0900

    image登録(filesystems)の修正

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,9 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => public_path(),
+            'url' => env('APP_URL') . '/img',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
@@ -50,7 +52,7 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'url' => env('APP_URL') . '/storage',
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
